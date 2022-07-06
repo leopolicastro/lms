dc975f576aafea8140a7d0887f53051e715597b4
Leo Policastro <leo.policastro@hey.com>
2022-07-06 02:39:46 -0400

    configure devise

--- app/controllers/users/unlocks_controller.rb
+++ app/controllers/users/unlocks_controller.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 
 class Users::UnlocksController < Devise::UnlocksController
+  layout "landing", only: [:new]
   # GET /resource/unlock/new
   # def new
   #   super
--- app/models/user.rb
+++ app/models/user.rb
@@ -36,7 +36,7 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-    :recoverable, :rememberable, :validatable, :trackable, :lockable
+    :recoverable, :rememberable, :validatable, :trackable
 
   has_one_attached :avatar
 
